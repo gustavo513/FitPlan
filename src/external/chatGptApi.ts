d4fb90a4b7d91c51185898db4ab0e867ba19de3a
Gustavo <gustavoortigoza06@gmail.com>
2025-10-05 13:03:47 -0300

    Servicio externo de OAuth2Client agregado

---
+++ src/external/chatGptApi.ts
@@ -0,0 +1,2 @@
+// En la carpeta external, van todos las APIs y los paquetes de terceros como servicios externos
+// Aquí se va a colocar la integración de la API de OPENAI
---
+++ src/utils/errorHandler.ts
@@ -0,0 +1,2 @@
+// En la carpeta utils van todas las funciones reutilizables en todo el proyecto
+// Aquí se va a colocar un manejador global de excepciones comunes en todo el proyecto
